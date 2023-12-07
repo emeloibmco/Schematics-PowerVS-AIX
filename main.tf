5e7091bad2fea2a30e7ec727fd073d46b0f23f8f
Juan Sebastian Rangel Araque <52113892+JuanRangelUN@users.noreply.github.com>
2023-12-07 11:30:32 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,55 +1,22 @@
-
-resource "ibm_pi_image" "powerimages"{
-  pi_image_name         = var.imagename
-  pi_image_id           = var.imageid
-  pi_cloud_instance_id  = var.powerinstanceid
-}
-
-data "ibm_pi_image" "ds_image" {
-  depends_on            = [ibm_pi_image.powerimages]
-  pi_image_name         = var.imagename
-  pi_cloud_instance_id  = var.powerinstanceid
-}
-
 resource "ibm_pi_key" "ssh_key" {
   pi_cloud_instance_id = var.powerinstanceid
   pi_key_name          = var.sshkeyname
   pi_ssh_key           = var.sshkey
 }
 
-resource "ibm_pi_network" "power_networks" {
-  count                = 1
-  pi_network_name      = var.networkname
-  pi_cloud_instance_id = var.powerinstanceid
-  pi_network_type      = "pub-vlan"
-  pi_cidr              = var.cidr
-  pi_dns               = ["127.0.0.1"]
-}
-
-data "ibm_pi_public_network" "ds_network" {
-  depends_on      = [ibm_pi_network.power_networks]
-  pi_cloud_instance_id = var.powerinstanceid
-}
-
-resource "ibm_pi_volume" "volume"{
-  pi_volume_size       = 10
-  pi_volume_name       = "test-volume"
-  pi_volume_type       = "tier3"
-  pi_volume_shareable  = true
-  pi_cloud_instance_id = var.powerinstanceid
-}
 
 resource "ibm_pi_instance" "test-instance" {
     pi_memory             = var.memory
     pi_processors         = var.processors
     pi_instance_name      = var.instancename
     pi_proc_type          = "shared"
-    pi_image_id           = data.ibm_pi_image.ds_image.id
-    pi_network {
-      network_id = data.ibm_pi_public_network.ds_network.id
-    }
-    pi_key_pair_name      = ibm_pi_key.ssh_key.key_id
+    pi_image_id           = 2582fbf5-1e58-4a0a-88d6-1fcb65289cdf
+    pi_key_pair_name      = ibm_pi_key.key.key_id
     pi_sys_type           = "s922"
-    pi_cloud_instance_id  = var.powerinstanceid
-    pi_volume_ids         = [ibm_pi_volume.volume.volume_id]
+    pi_cloud_instance_id  = "51e1879c-bcbe-4ee1-a008-49cdba0eaf60"
+    pi_pin_policy         = "none"
+    pi_health_status      = "WARNING"
+    pi_network {
+      network_id = 7adb57fd-0f43-4ab8-981e-36bf62c3592b
+    }
 }
