78f5bbd6767221ed41e078219d66d7430510a108
Juan Sebastian Rangel Araque <52113892+JuanRangelUN@users.noreply.github.com>
2023-12-14 09:40:50 -0500

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,5 +1,5 @@
 variable "ibmcloud_api_key" {
-  default     = "cySbCuhe9Dm5-2RCf9n0jz_R3P5eGyWy9aJxh0YMlswK"
+  default     = ""
   description = "API Key to provisioning resources to the account"
 }
 variable "ibm_region" {
@@ -13,7 +13,7 @@ variable "sshkeyname" {
 }
 
 variable "sshkey" {
-  default="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwjA7FCebg+xe5RlPLGsQDmFt8ShQUce8icxDMAQHKvgYyOogxh5EPc9F493wqrxSDnQNx8ca9XKRDZxuemwpBj7dzDZLgIFw8Bu5CfEAy1dz9+Q8kilccsi5zVpPggCD1Y8O5yLajRAWsa10jvZe3VUfPeJjDh9U0H0/Y84S86+BtXKG9q2HkvYrgO2q5jaLVRW16vY/w2VvH3V43+40JQ6viU5BCRE4BWqxwcTE/LoK+9amaHczVvzw+sVt/15oPl2/YXNgdsm7+DuAdYELBEtRcKONQ5aAiMp453q13huko0CgBauXUgermdJXBc3EGnp4NZ8qrlDLuMWDpy8gJ2/3PsS44Ah3vQoASh4mqilQIG5bx0tj0ujAE3EjWk+b5CJmzv5L5BHr5nLMeEL/8jFq25wZqqgBVcV8+ZBlM/c71OYtZk7qSjr7CcvCukUeArXomLDTZG9fUAvoZ6yL6y4yphR8gweM3Kl5XNL6Q+DDsM84IJUC0Vpp0Oy5qEfc="
+  default=""
   description = "Public ssh key"
 }
 
